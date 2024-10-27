6c53508ffd449dfaac30b642f805c1167c472ea4
vinayak19th <vinayak1998th@gmail.com>
2024-10-27 12:30:20 -0700

    Added Lecture 2.1 Code

--- Week1/utils.py
+++ Week1/utils.py
@@ -15,15 +15,32 @@ def draw_and_get_state(gates):
     print(f"State created: \n {state}")
     plt.show()
 
-@qml.qnode(dev)
+@qml.qnode(dev,grad_on_execution=False,diff_method=None)
 def circuit(gates:list):
     for g in gates:
         g(0)
     return qml.probs(0)
 
-@qml.qnode(dev)
+@qml.qnode(dev,grad_on_execution=False,diff_method=None)
 def probelm_circuit(gates:list):
     for g in gates:
         g(0)
     return qml.state()
 
+# Lecture 2
+@qml.qnode(dev,grad_on_execution=False,diff_method=None)
+def circuit_ops(ops):
+    for op in ops:
+        qml.apply(op)
+    return qml.state()
+
+def apply_ops_and_draw(ops):
+    print(f"State Created by Decomposition:\n {circuit_ops(ops)}")
+    qml.draw_mpl(circuit_ops)(ops)
+    plt.show()
+
+def decompose_and_plot(gate):
+    gate_wire=gate(0)
+    draw_and_get_state([gate])
+    print(f"Decomposition:\n {gate_wire.decomposition()}")
+    apply_ops_and_draw(gate_wire.decomposition())
